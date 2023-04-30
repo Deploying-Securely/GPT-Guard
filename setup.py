0573b6c677a2a9ebb2cbf641ab440075621647da
Walter-Haydock <41760518+Walter-Haydock@users.noreply.github.com>
2023-04-30 10:08:54 -0400

    created

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="gpt_guard",
+    version="1.0.0",
+    author="Walter Haydock",
+    author_email="walter@deploy-securely.com",
+    description="A lightweight library to sanitize data provided to AI tools",
+    long_description="README.md",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Deploying-Securely/GPT-Guard",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=['License :: OSI Approved :: Apache Software License'],
+)
